cc14299245755f615cd24d2c2c4627e1226c6f6e
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2018-08-19 16:51:04 +0900

    phinaを諦める

--- page/phinajs/main.js
+++ page/phinajs/main.js
@@ -1,8 +1,4 @@
-var phi_obj = [];
-var data = [];
-data.state = [];
-data.state.limit = Date.now();
-var timer;
+var viewdata = new ViewData();
 
 var client = new Paho.MQTT.Client("127.0.0.1", 9090, "clientId");
 client.onConnectionLost = onConnectionLost;
@@ -25,8 +21,8 @@ function onConnectionLost(responseObject)
 function onMessageArrived(message)
 {
 	console.log("onMessageArrived");
-	console.log(phi_obj.label)
-	phi_obj.label.text = "arrived";
+	console.log(phi_obj.label);
+	viewdata.update(message.payloadString);
 };	
 
 // phina.js をグローバル領域に展開
@@ -45,21 +41,7 @@ phina.define('MainScene', {
     timer.y = this.gridY.center(); // y 座標
     timer.fill = 'white'; // 塗りつぶし色
     timer.update = function(){
-	var millisec = Math.floor( data.state.limit*1000 - (Date.now()) );
-	var timestr = millisec2str(millisec);
-	if(flg){
-		console.log(timestr);
-		flg = false;
-	}
-
-	if(millisec <= 0)
-	{
-		this.text = "00:00.00";
-		clearInterval(timer);
-	}else
-	{
-		this.text = timestr;
-	}
+	this.text = viewdata.getCompeTimeStr();
     }
 
     //四角を生成
@@ -84,55 +66,3 @@ phina.main(function() {
   app.run();
 });
 
-function updatePoint(jsontxt)
-{
-	data = JSON.parse(jsontxt);
-	console.log(data);
-
-	timer = setInterval(updateTime,10);
-
-	document.getElementById("point_box").innerHTML = data.state.point;
-	for(var i=0;i<4;i++)
-	{
-		document.getElementById("base"+i).innerHTML = data.state.base[i];
-	}
-}
-var flg = true;
-function updateTime()
-{
-	var millisec = Math.floor( data.state.limit*1000 - (Date.now()) );
-	var timestr = millisec2str(millisec);
-	if(flg){
-		console.log(timestr);
-		flg = false;
-	}
-
-	if(millisec <= 0)
-	{
-		document.getElementById("time_area").innerHTML = "00:00.00";
-		clearInterval(timer);
-	}else
-	{
-		document.getElementById("time_area").innerHTML = timestr;
-	}
-}
-
-function millisec2str(time)
-{
-	var m = Math.floor(time / (1000*60));
-	time -= m*(1000*60);
-
-	var s = Math.floor(time / 1000);
-	time -= s*1000;
-
-	var ms = Math.floor(time / 10);
-	return fillzero(m)+":"+fillzero(s)+"."+ms;
-}
-
-function fillzero(n)
-{
-	if(n<10)
-		return "0"+n;
-	else
-		return ""+n;
-}
